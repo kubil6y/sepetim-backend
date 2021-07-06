c4e54663be2ce13ea338f903dc4b5f226641e179
Kubilay Humm <lieqb2@gmail.com>
2021-07-06 12:17:58 +0300

    PORT

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new TrimInputType());
   app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(5000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
